1a927b87fb7fb3476385de273322db601d08849d
Solidmatrix <30371270+Solidmatrix@users.noreply.github.com>
2017-12-05 17:53:59 +0800

    Delete 43. Multiply Strings.py

--- 43. Multiply Strings.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def multiply(self, num1, num2):
-        """
-        :type num1: str
-        :type num2: str
-        :rtype: str
-        """
-        return str(int(num1) * int(num2))
